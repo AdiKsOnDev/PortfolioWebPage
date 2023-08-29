743fb8dd0553a969d162b75de757d30e8c2cbc67
AdiKsOnDev <adilalizade13@gmail.com>
2023-08-29 21:34:56 +0400

    Created an API route that uses POST method

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { DataBaseConnect } from "./DataBaseConnect.js";
-import Project from "./Models.js";
+import { Project } from "./Models.js";
+import { Review } from "./Models.js";
 
 dotenv.config();
 DataBaseConnect();
@@ -25,6 +26,27 @@ app.get("/projects", async(req, res) => {
         res.status(404).json({ msg: "Data could not be found." });
     }
 });
+
+app.post("/", async(req, res) => {
+    try {
+        const {email, name, review} = req.body;
+        const newReview = {
+            email,
+            name,
+            review,
+        };
+
+        if (newReview) {
+            const review = await Review.create(newReview);
+            res.status(201).json(review);
+        }
+    } catch (error) {
+        res.status(404).json({
+            message: "Sorry, something is wrong with your data.",
+            err: error.message
+        });
+    }
+})
 
 app.listen(port, () => {
     console.log(`Listening to the server at http://localhost:${port}`);
