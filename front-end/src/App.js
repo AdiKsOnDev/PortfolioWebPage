8d47cf9e5cdc85782f15013f1349990cefb66ab2
AdiKsOnDev <adilalizade13@gmail.com>
2023-11-05 23:43:05 +0400

    Made the Header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className="bg-primary h-screen w-screen">
+    <div className="h-screen w-screen bg-primary fixed">
       <Header />
     </div>
   );
