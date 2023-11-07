5a1ff6257328048aa4a3ca60cd72aba2310f2d34
AdiKsOnDev <adilalizade13@gmail.com>
2023-11-07 15:15:19 +0400

    Added github ling to projects

--- front-end/src/components/Project.js
+++ front-end/src/components/Project.js
@@ -1,3 +1,7 @@
+import { ReactComponent as GitHub } from '../Assets/GitHub\ Logo.svg';
+import { ReactComponent as GitHubLogo } from '../Assets/GitHub Logo.svg';
+
+
 function Project(props) {
     return ( 
         <div className="flex flex-col w-fit mx-12 h-fit justify-center items-start p-6 mt-10 rounded-2xl bg-secondary">
@@ -6,6 +10,9 @@ function Project(props) {
             <div classname="w-full">
                 <p className="font-normal text-sm text-tertiary text-left w-3/4 mt-4">{props.description}</p>
             </div>
+            <a href={props.gitRepo} target="_blank" rel="noopener noreferrer">
+                <GitHub className="w-6 mr-2 hover:accent-accent" />
+            </a>
         </div>
     );
 }
