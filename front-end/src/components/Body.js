9b68007271045d99fb62a5da70ebdb9ac169c811
AdiKsOnDev <adilalizade13@gmail.com>
2023-11-13 01:28:25 +0400

    Review Box

--- front-end/src/components/Body.js
+++ front-end/src/components/Body.js
@@ -1,11 +1,13 @@
 import About from "./About";
 import Projects from "./Projects";
+import Review from "./Review";
 
 function Body() {
     return ( 
         <div className="max-w-screen">
             <Projects />
             <About />
+            <Review />
         </div>
     );
 }
---
+++ front-end/src/components/Review.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function Review() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Email: ${email}, Name: ${name}, Description: ${description}`);
+    };
+
+    return (
+        <div className="w-full flex flex-col justify-center items-center p-24">
+            <h1 className="Review text-tertiary font-semibold text-5xl mb-5">Leave a Review</h1>
+            
+            <form className="flex flex-col justify-center items-center w-full bg-secondary rounded-2xl p-12" onSubmit={handleSubmit}>
+                <input className='w-full rounded-md shadow-inner px-5 py-2 mb-5' type="text" value={name} placeholder='Name' onChange={(e) => setName(e.target.value)} />
+
+                <input className='w-full rounded-md shadow-inner px-5 py-2 mb-5' type="email" value={email} placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+
+                <textarea className='w-full rounded-md shadow-inner px-5 py-2 h-24 mb-5' value={description} placeholder='Description' onChange={(e) => setDescription(e.target.value)} />
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Review;
